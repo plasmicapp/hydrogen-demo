7dfa5b185c860bd073112f64f69028f27f8695cb
Chung Wu <chungwu@plasmic.app>
2021-11-17 18:59:14 -0800

    useClientShopQuery does its own caching instead
    The cache for useQuery is not initialized...  Hmm, not sure how
    to do so from the client 🤔

--- src/hooks/useClientShopQuery.client.js
+++ src/hooks/useClientShopQuery.client.js
@@ -1,25 +1,38 @@
-import { fetchBuilder, graphqlRequestBody, useQuery } from "@shopify/hydrogen/client";
+import { fetchBuilder, graphqlRequestBody } from "@shopify/hydrogen/client";
 import shopifyConfig from '../../shopify.config';
+
+const cache = {};
 
 export function useClientShopQuery({
   query, variables
 }) {
   const {storeDomain, storefrontToken, graphqlApiVersion } = shopifyConfig;
   const body = graphqlRequestBody(query, variables);
-  const url = `https://${storeDomain}/api/${graphqlApiVersion}/graphql.json`;
-  const request = new Request(url, {
-    method: 'POST',
-    headers: {
-      'X-Shopify-Storefront-Access-Token': storefrontToken,
-      'content-type': 'application/json',
-    },
-    body,
-  });
-
-  const {data} = useQuery(
-    [storeDomain, graphqlApiVersion, body],
-    fetchBuilder(request)
-  );
-
-  return data;
+  
+  const key = `${storeDomain}-${graphqlApiVersion}-${body}`;
+  if (!(key in cache)) {
+    let promise;
+    let result;
+    cache[key] = () => {
+      if (result) {
+        return result;
+      }
+      if (!promise) {
+        const url = `https://${storeDomain}/api/${graphqlApiVersion}/graphql.json`;
+        const request = new Request(url, {
+          method: 'POST',
+          headers: {
+            'X-Shopify-Storefront-Access-Token': storefrontToken,
+            'content-type': 'application/json',
+          },
+          body,
+        });
+        promise = fetchBuilder(request)().then(data => {
+          result = data;
+        });
+        throw promise;
+      }
+    };
+  }
+  return cache[key]();
 }
